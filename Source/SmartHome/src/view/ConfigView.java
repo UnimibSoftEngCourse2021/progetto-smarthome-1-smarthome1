66dd78a1c4146cdc1fb360ffaca19187af789475
Federico Benaglia <f.benaglia2@campus.unimib.it>
2021-02-17 00:16:01 +0100

    -finita implementazione heatSystemConfig della classe ConfigView

--- Source/SmartHome/src/view/ConfigView.java
+++ Source/SmartHome/src/view/ConfigView.java
@@ -6,6 +6,8 @@ import application.RoomFaçade;
 
 public class ConfigView {
 	
+	private GenericFaçade genericFacade;
+	private DataFaçade dataFacade;
 	Scanner input = new Scanner(System.in);
 
 	public void roomConfig() {
@@ -14,10 +16,10 @@ public class ConfigView {
 		do {
 			System.out.println("Inserire il nome della stanza che si intende creare: ");
 			roomName = input.nextLine();
-			if(!RoomFaçade.manageRoomConfig(roomName)) {
+			if(!dataFacade.manageRoomConfig(roomName)) {
 				System.out.print("Nome non disponibile");
 			}
-		}while(!RoomFaçade.manageRoomConfig(roomName));
+		}while(!dataFaçade.manageRoomConfig(roomName));
 				
 		System.out.println("Inserire il piano della stanza: ");
 		roomFloor = input.nextLine();
@@ -110,35 +112,104 @@ public class ConfigView {
 		int tempPassiva;
 		int tempAttiva;
 		String giorno;
-		do {
-			System.out.println("Inserire la temperatura passiva: ");
-			tempPassiva = input.nextInt();
-			if(tempPassiva < 7 || tempPassiva > 40) 
-				System.out.print("La temperatura inserita deve essere compresa tra 7 e 40 C");
-		}while(tempPassiva < 7 || tempPassiva > 40);
-		do {
-			System.out.println("Inserire la temperatura attiva: ");
-			tempAttiva = input.nextInt();
-			if(tempAttiva < 7 || tempAttiva > 40) 
-				System.out.print("La temperatura inserita deve essere compresa tra 7 e 40 C");
-		}while(tempAttiva < 7 || tempAttiva > 40);
+		double sceltaTemp;
+		//double[][] matrix = new double[7][23];
+		double[] setProgDay = new double[26];
+		// controlla che l'utente ha inserito almeno la tempAttiva Passiva di tutti i giorni
+		boolean[] checkProgDay = new boolean[7];
+		
+		for(int i=0; i < checkProgDay.length; i++)
+			checkProgDay[i] = false;
 		
 		
-			System.out.println("Scegliere il giorno della settimana che si vuole programmare: ");
-			giorno = input.nextLine();
 			boolean fine = false;
-			while(!fine)
-				switch(giorno) {
-				case "lunedi":
-				case "martedi":
-				case "mercoledi":
-				case "giovedi":
-				case "venerdi'":
-				case "sabato":
-				case "domenica":
-					for(int i = 0; i < 24, i++) {
-						System.out.println("Scegliere la ")
+			boolean notAllProg;
+			while(!fine) {
+				do {
+				notAllProg = false;
+				
+				System.out.println("Scegliere il giorno della settimana che si vuole programmare: ");
+				giorno = input.nextLine();
+			
+				//switch(giorno) {
+				//case "lunedi":
+				//case "martedi":
+				//case "mercoledi":
+				//case "giovedi":
+				//case "venerdi'":
+				//case "sabato":
+				//case "domenica":
+					do {
+						System.out.println("Inserire la temperatura passiva: ");
+						tempPassiva = input.nextInt();
+						if(tempPassiva < 7 || tempPassiva > 40) 
+							System.out.print("La temperatura inserita deve essere compresa tra 7 e 40 C");
+					}while(tempPassiva < 7 || tempPassiva > 40);
+					
+					do {
+						System.out.println("Inserire la temperatura attiva: ");
+						tempAttiva = input.nextInt();
+						if(tempAttiva < 7 || tempAttiva > 40) 
+							System.out.print("La temperatura inserita deve essere compresa tra 7 e 40 C");
+					}while(tempAttiva < 7 || tempAttiva > 40);
+					
+					setProgDay[0] = tempPassiva;
+					setProgDay[1] = tempAttiva;
+					
+					for(int i = 2; i < setProgDay.length; i++) 
+						setProgDay[i] = 0;
+					
+					System.out.println("Inserire la programmazione per ogni ora: ");
+					for(int i = 2; i < setProgDay.length; i++) {
+						do {
+							System.out.println("Inserire 0 per temperatura Passiva o 1 per Attiva per le ore: " + i);
+							setProgDay[i] = input.nextDouble();
+							if(sceltaTemp != 0 || sceltaTemp != 1)
+								System.out.println("Valore non valido!");
+						}while(sceltaTemp != 0 || sceltaTemp != 1);
 					}
-				}	
+					
+					genericFacade.manageWriteOnFile(giorno, setProgDay);
+					
+						switch(giorno) {
+						case "lunedi":
+							checkProgDay[0] = true;
+							break;
+						case "martedi":
+							checkProgDay[1] = true;
+							break;
+						case "mercoledi":
+							checkProgDay[2] = true;
+							break;
+						case "giovedi":
+							checkProgDay[3] = true;
+							break;
+						case "venerdi'":
+							checkProgDay[4] = true;
+							break;
+						case "sabato":
+							checkProgDay[5] = true;
+							break;
+						case "domenica":
+							checkProgDay[6] = true;
+							break;
+						}
+					
+					for(int i = 0; i < checkProgDay.length; i++) {
+						if(checkProgDay[i] == false) {
+							notAllProg = true;
+							break;
+						}
+					}
+					if(notAllProg == true)
+						System.out.println("Inserire i giorni rimanenti");
+					
+					
+				}while(!notAllProg);
+				
+				System.out.print("continuare con la programmazione dei giorni? (s/n)");
+				if(input.nextLine().equalsIgnoreCase("n"))
+					fine = true;
+			}
 	}
 }
