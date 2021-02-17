b0a82115301c492590519961e34c5ea7153fe5ee
Lorenzo Basavecchia <l.basavecchia@campus.unimib.it>
2021-02-17 18:16:51 +0100

    aggiunta gestione menù

--- Source/SmartHome/src/view/Menu.java
+++ Source/SmartHome/src/view/Menu.java
@@ -7,73 +7,79 @@ import java.util.Scanner;
 
 import domain.Object;
 
-
-
 public class Menu {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Selezionare l'operazione desiderata scrivendo la parola chiave tra quelle presentate qui sotto");
-		System.out.println("config: configurazione dei parametri relativi al sistema");
-		System.out.println("scenario: attivazione, modifica e eliminazione di scenari");
-		System.out.println("stati: visualizzare informazioni relative agli stati degli oggetti");
-		System.out.println("azioni: effettuare manualmente azioni sugli oggetti");
-		System.out.println("isAtHome: impostare lo stato di home oppure di away");
-
 		Scanner input = new Scanner(System.in);
-		switch(input.nextLine()) {
-		case "config":
-			String config;
-			ConfigView configView = new ConfigView();
-			List<String[]> numHeaterNotBinded = new ArrayList<String[]>();
-			List<String[]> sensorList = new ArrayList<String[]>();
-			
-			configView.alarmConfig();
-			configView.dayMode();
-			do {
-				String room = configView.roomConfig();
-				String floor = configView.floorConfig();
-				System.out.println("Inserire gli oggetti e sensori che si vuole inserire");
-				System.out.println("Oggetti: light, window, door, heater");
-				System.out.println("Sensori: air, movement, temperature");
-				config = input.nextLine(); 
-				switch(config) {
-				case "light":
-					configView.lightConfig();
-					break;
-				case "window":
-					configView.windowConfig();
-					break;
-				case "door":
-					configView.doorConfig();
-					break;
-				case "heater":
-					numHeaterNotBinded = configView.heaterConfig(room, floor, numHeaterNotBinded);
-					break;
-				case "air":
-					configView.airSensorConfig();
-					break;
-				case "movement":
-					configView.movementSensorConfig();
-					break;
-				case "temperature":
-					sensorList = configView.temperatureSensorConfig(room, sensorList);
-					break;
-				}
-				System.out.println("Inserire altre stanze? (s/n)");
-				config = input.nextLine();
+		
+		ConfigView configView;
+		ScenarioView scenarioView;
+		ObjectStateView objectStateView;
+		ManualActionView manualActionView;
+		IsAtHome isAtHome;
+		
+		do {
+			System.out.println("Selezionare l'operazione desiderata scrivendo la parola chiave tra quelle presentate qui sotto");
+			System.out.println("config: configurazione dei parametri relativi al sistema");
+			System.out.println("heat: configurazione sistema di riscaldamento");
+			System.out.println("scenario: attivazione, modifica e eliminazione di scenari");
+			System.out.println("stati: visualizzare informazioni relative agli stati degli oggetti");
+			System.out.println("azioni: effettuare manualmente azioni sugli oggetti");
+			System.out.println("isAtHome: impostare lo stato di home oppure di away");
+		
+			switch(input.nextLine()) {
+			case "config":
+				String config;
+				configView = new ConfigView();
+				List<String[]> numHeaterNotBinded = new ArrayList<String[]>();
+				List<String[]> sensorList = new ArrayList<String[]>();
 				
-			}while(config.equalsIgnoreCase("s"));
-			
-			
+				configView.alarmConfig();
+				configView.dayMode();
+				do {
+					String room = configView.roomConfig();
+					String floor = configView.floorConfig();
+					System.out.println("Inserire gli oggetti e sensori che si vuole inserire");
+					System.out.println("Oggetti: light, window, door, heater");
+					System.out.println("Sensori: air, movement, temperature");
+					config = input.nextLine(); 
+					switch(config) {
+					case "light":
+						configView.lightConfig();
+						break;
+					case "window":
+						configView.windowConfig();
+						break;
+					case "door":
+						configView.doorConfig();
+						break;
+					case "heater":
+						numHeaterNotBinded = configView.heaterConfig(room, floor, numHeaterNotBinded);
+						break;
+					case "air":
+						configView.airSensorConfig();
+						break;
+					case "movement":
+						configView.movementSensorConfig();
+						break;
+					case "temperature":
+						sensorList = configView.temperatureSensorConfig(room, sensorList);
+						break;
+					}
+					System.out.println("Inserire altre stanze? (s/n)");
+					config = input.nextLine();
+					
+				}while(config.equalsIgnoreCase("s"));
+				
+				
 				// questo controllo lo fa alla fine dell'inserimento di ogni stanza
 				System.out.print("Associare i sensori temp creati con gli oggetti heater: ");
 				// se ho ancora caloriferi da associare
-				do {
+				
 					for(String[] sensor: sensorList) {
 						genericFacade.manageWriteOnFile("temperature", sensor[0]);
-						genericFacade.manageWriteOnFile("roomNameSensor", sensor[1]);
-						
+						genericFacade.manageWriteOnFile("roomNameSensor", sensor[1]);						
 						do {
 							if(!numHeaterNotBinded.isEmpty()) {
 								System.out.println("Caloriferi disponibili: ");
@@ -96,27 +102,33 @@ public class Menu {
 									System.out.println("Vuoi associare altri caloriferi al sensore " + sensor[0] +" ?");
 									config = input.nextLine();
 								}
-							}
-							
-						}while(config.equalsIgnoreCase("s") || !numHeaterNotBinded.isEmpty());
-					}	
-					if(!numHeaterNotBinded.isEmpty())
-						System.out.println("Alcuni caloriferi devono essere ancora associati");
-				}while(!numHeaterNotBinded.isEmpty());	
-					
-			break;
-		case "scenario":
-			break;
-		case "stati":
-			break;
-		case "azioni":
-			break;
-		case "isAtHome":
-			break;
-		default:
-			break;	
-		}
-		input.close();
+							}							
+						} while(config.equalsIgnoreCase("s") || !numHeaterNotBinded.isEmpty());
+						genericFacade.manageWriteOnFile("heaterID", "");
+					}
+					genericFacade.manageWriteOnFile("end", null);							
+				break;
+			case "heat":
+				configView.heatSystemConfig();
+				break;
+			case "scenario":
+				scenarioView.selectOperation();
+				break;
+			case "stati":
+				objectStateView.getObjectState();
+				break;
+			case "azioni":
+				manualActionView.performAction();
+				break;
+			case "isAtHome":
+				isAtHome.changeFlag();
+				break;
+			default:
+				System.out.println("Input non valido");
+				break;	
+			}		
+			System.out.println("Si desidera effettuare altre operazioni? (s/n)");		
+		} while (input.nextLine().equals("n"));
+			input.close();
 	}
-
 }
